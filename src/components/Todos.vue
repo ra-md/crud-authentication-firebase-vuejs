a9a9d7b5bb766b8b8d9982b2937b57907fc97d82
7yk <ra799m@gmail.com>
2020-01-29 20:52:48 +0700

    new file:   src/components/Todos.vue 	new file:   src/firebase.js

---
+++ src/components/Todos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- name filenya ?????? -->
+  </div>
+</template>
+
+<script>
+// import TodoList from './TodoList.vue';
+import { todoCollection } from '@/firebase.js';
+
+export default {
+  components: {
+    // TodoList
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  created() {
+      todoCollection.onSnapshot(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          let obj = {
+            id: doc.id,
+            title: doc.data().title,
+            completed: doc.data().completed
+          }
+          this.todos.push(obj)
+        })
+      })
+  }
+}
+</script>
